c72e8ab107f6d75c3a6618865862e739a88fbe94
Arun11111111 <arunch4320@gmail.com>
2026-04-05 10:56:57 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+let gameSeq=[];
+let userSeq=[];
+
+let started=false;
+let level=0;
+
+let btns =["yellow","red","purple","green"];
+let h2=document.querySelector("h2");
+document.addEventListener("keypress",function(){
+    if(started==false){
+    console.log("game started");
+    started = true;
+    levelUp();
+
+    }
+})
+   function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250);}
+    function userFlash(btn){
+        btn.classList.add("flash");
+    setTimeout(function(){
+            btn.classList.remove("flash");
+        },250);
+}
+function levelUp(){
+    userSeq=[];
+    level++;
+h2.innerText=`level ${level}`;
+
+let randomIdx=Math.floor(Math.random()*3);
+let randColor=btns[randomIdx];
+let randBtn=document.querySelector(`.${randColor}`);
+
+gameSeq.push(randColor);
+console.log(gameSeq);
+gameFlash(randBtn);
+}
+function checkAns(idx){
+ if(userSeq[idx]==gameSeq[idx]){
+ if(userSeq.length==gameSeq.length){
+   setTimeout( levelUp(),1000);
+ }
+    }
+ else{
+        h2.innerHTML=`Game Over! Your score was <b>${level}</b> <br> press any key to start `;
+        reset();
+    }
+}
+
+function btnPress(){
+btn = this;
+userFlash(btn);
+
+ let userColor = btn.getAttribute("id");
+userSeq.push(userColor);
+
+checkAns(userSeq.length-1);
+}
+
+
+let allBtns=document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
+}
+function reset(){
+    started=false;
+    gameSeq=[];
+    userSeq=[];
+    level =0;
+}
